54a0c87897ee8c0f465e41d34ea1ce6e47ab5fd6
vsmith8 <vsmith8@hawk.iit.edu>
2024-11-05 19:12:07 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 function init(){
 //add your javascrip between these two lines of code
+  var button = document.getElementById('entrybutton');
  
+  function showMeText(){
+    var textbox = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = textbox.value;
+    alert("Tory Smith: " + textbox.value);
+  }
 
-
-
-
-
-
-
+  button.addEventListener('click', showMeText);
+  }
+ 
 window.addEventListener('load', init);
